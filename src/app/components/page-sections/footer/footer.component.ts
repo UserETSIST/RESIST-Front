34b1c17d6fa6048f4f0679f43502154ff0381dd9
UserETSIST <david.dima@alumnos.upm.es>
2025-02-10 00:28:30 +0100

    Subscribe to newslwetter --> DONE

--- src/app/components/page-sections/footer/footer.component.ts
+++ src/app/components/page-sections/footer/footer.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { RouterLink } from '@angular/router';
 import { NewsletterService } from '../../../services/newsletter.service';
-import {FormsModule} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators} from '@angular/forms';
 import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, FormsModule ],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
@@ -16,14 +17,17 @@ export class FooterComponent {
 
   GfRLogo = environment.GFR_LOGO;
 
+  subscribeToNewsLetterForm = new FormGroup ({
+    email: new FormControl('', [Validators.required,Validators.email, this.forbiddenCodeValidator])
+  })
 
-  email: string = '';
+
 
   constructor(private newsletterService: NewsletterService) {}
 
   onSubscribe(): void {
-    if (this.email) {
-      this.newsletterService.subscribeUser(this.email).subscribe({
+    if (this.subscribeToNewsLetterForm.valid) {
+      this.newsletterService.subscribeUser(this.subscribeToNewsLetterForm.value.email!).subscribe({
         next: (response) => {
           Swal.fire({
             position: "top-end",
@@ -46,6 +50,23 @@ export class FooterComponent {
         }
       });
     }
+  }
+
+
+  forbiddenCodeValidator(control: AbstractControl): ValidationErrors | null {
+    const forbiddenPatterns = [
+      /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,  // Detecta etiquetas <script>
+      /\b(SELECT|INSERT|UPDATE|DELETE|DROP|--)\b/gi,          // Detecta palabras clave SQL
+      /(\%27)|(\')|(\-\-)|(\%23)|(#)/gi,                      // Detecta caracteres de inyección SQL (' -- #)
+    ];
+  
+    for (const pattern of forbiddenPatterns) {
+      if (pattern.test(control.value)) {
+        return { forbiddenCode: true };
+      }
+    }
+  
+    return null;  
   }
 
 }
